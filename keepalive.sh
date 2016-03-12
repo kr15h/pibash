319a6051572ec6a797440782987be499c5c4b08c
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-03-12 21:45:37 +0100

    Improve in-file readme

--- keepalive.sh
+++ keepalive.sh
@@ -1,18 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
-# keepalive.sh
-# Basic script to keep alive your process.
-# Instructions
-# Replace myprocess in the script with your actual process name.
-# Save the file as keepalive.sh somewhere, /home/pi/keepalive.sh is a good choice.
-# Set up cron task by editing crontab with crontab -e. 
-# Add * * * * * /home/pi/keepalive.sh - the script will be run once a minute 
-# and restart your process if it has failed.
+# Name:         keepalive.sh
+# Description:  Basic script to keep alive your process.
+# Instructions: Replace myprocess in the script with your actual process name.
 
 case "$(pidof myprocess | wc -w)" in
 
 0)  echo "Restarting My Process:     $(date)" >> /var/log/myprocess.txt
-    /home/pi/myprocess -and -some -args &
+    /path/to/myprocess &
     ;;
 1)  # all ok
     ;;
