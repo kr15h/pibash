e71df6df9fcabace1a13eef2d9d1ff3e96b96f25
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-03-12 21:45:52 +0100

    Improve in-file readme of `toggle.sh`

--- toggle.sh
+++ toggle.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-# Each minute connect to different camera stream with omxplayer.
-# Open crontab with `crontab -e` and add the following:
-# * * * * * /home/pi/pibash/toggle.sh
-# It will run this script every minute.
+
+# Name:         toggle.sh
+# Description:  Do one thing if current minute is an even number, Something else if odd.
+# Instructions: Replace all occurences of process-a and process-b with your own.
 
 MINS=$(date +"%M")
 
 if (( $MINS % 2 == 0 ))
 then
-    sudo killall omxplayer.bin
-    sudo killall omxplayer
-    # Connect to epilog laser camera
-    omxplayer rtsp://@172.26.2.160:8554/ &
+    sudo killall process-b
+    /path/to/process-a &
 else
-    sudo killall omxplayer.bin
-    sudo killall omxplayer
-    # Connect to CNC machine camera
-    omxplayer rtsp://@172.26.2.153:8554/ &
+    sudo killall process-a
+    /path/to/process-b &
 fi
 
 unset MINS
