9ed1326ce2efa8f5bbaba82d7abf6d8677f86007
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-08-26 19:24:45 +0300

    Add crontab instructions to 'keepalive.sh'

--- keepalive.sh
+++ keepalive.sh
@@ -3,6 +3,13 @@
 # Name:         keepalive.sh
 # Description:  Basic script to keep alive your process.
 # Instructions: Replace myprocess in the script with your actual process name.
+# Add the script to crontab. Use 'crontab -e' to edit your cron file.
+# Add the following lines.
+#
+# @reboot /home/pi/pibash/keepalive.sh
+# * * * * * /home/pi/pibash/keepalive.sh
+#
+# This will run 'keepalive.sh' on reboot and every minute.
 
 case "$(pidof myprocess | wc -w)" in
 
